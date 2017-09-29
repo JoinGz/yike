76a6bbe28b1e1eae8c6a6c353231e25cacb3bb1d
Your Name <you@example.com>
2017-09-29 15:33:22 +0800

    完成了侧边栏的划入划出

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,39 @@
-var app = angular.module("App",["hehe"]);
+var app = angular.module("App",["hehe",'ngRoute',]);
+app.run(["$rootScope",function($rootScope){
+	$rootScope.done = false;
+	$rootScope.fn=function(){
+		$rootScope.done = !$rootScope.done ;
+		var a = document.getElementsByClassName('viewport')[0].firstElementChild;
+		var a1 = a.firstElementChild;
+		var navs = a1.getElementsByTagName('dd');
+		if($rootScope.done){
+//			for(var i=0;i<navs.length;i++){
+//				navs[i].style.transform = "translate(0)";
+//				navs[i].style.transitionDelay="0.2s";
+//				navs[i].style.transitionDuration=(i + 1) * 0.15 + 's';
+//			};
+			for(var i=0; i<navs.length; i++) {
+				navs[i].style.transform = 'translate(0)';
+				navs[i].style.transitionDelay = '0.2s';
+				navs[i].style.transitionDuration = (i + 1) * 0.15 + 's';
+			}
+		}else{
+			var len = navs.length - 1;
+				for(var j=len; j>0; j--) {
+				// console.log(navs.length - j + 1);
+				navs[j].style.transform = 'translate(-100%)';
+				navs[j].style.transitionDelay = '';
+				navs[j].style.transitionDuration = (len - j) * 0.15 + 's';
+			}
+		}
+		
+	}
+}]);
+app.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider.when("/today",{
+		templateUrl:"./views/today.html",
+		controller:"control"
+	}).when("/older",{
+		templateUrl:"./views/older.html",
+	})
+}]);
--- scripts/control.js
+++ scripts/control.js
@@ -1,3 +1,3 @@
 angular.module("hehe",[]).controller("control",["$scope",function($scope){
-	$scope.txt = "123";
+	
 }])
